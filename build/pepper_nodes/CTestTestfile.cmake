d4b5f3f8accef3a70950c92182d8c7c1b086aaaa
Nunzio Del Gaudio <92187901+DelGaudioNunzioSE@users.noreply.github.com>
2024-12-12 16:24:53 +0100

    Delete build directory

--- build/pepper_nodes/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sirc/Scrivania/project_ws/src/cogrob_pepper_nodes/src/pepper_nodes
-# Build directory: /home/sirc/Scrivania/project_ws/build/pepper_nodes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
