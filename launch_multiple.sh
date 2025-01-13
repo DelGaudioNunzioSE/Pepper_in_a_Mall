857ae8d9faaa7ca1f192168646ba4244229b9b42
nunzi <delgaudionunziose@gmail.com>
2025-01-13 17:59:27 +0100

    readded

--- launch_multiple.sh
+++ launch_multiple.sh
@@ -7,6 +7,19 @@
 
 roslaunch ros_audio_pkg speech_recognition.launch &
 
+#!/bin/bash
+
+# Lista di comandi da eseguire
+commands=(
+  "roslaunch 'ros_audio_pkg speech_recognition.launch '"
+  "roslaunch pepper_nodes pepper_bringup.launch"
+  "top"
+)
+
+# Esegui ogni comando in un nuovo terminale
+for cmd in "${commands[@]}"; do
+  gnome-terminal -- bash -c "$cmd; exec bash"
+done
 
 #roslaunch your_package second_launch_file.launch &
 
