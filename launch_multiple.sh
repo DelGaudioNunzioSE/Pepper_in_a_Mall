76cedaa1eb6d025641b51b56a1e891193814d7ea
nunzi <delgaudionunziose@gmail.com>
2025-01-14 00:17:51 +0100

    starting_multiple launch file

--- launch_multiple.sh
+++ launch_multiple.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-
-#chmod +x launch_multiple.sh
-#./launch_multiple.sh
-
-
-roslaunch ros_audio_pkg speech_recognition.launch &
-
-#!/bin/bash
-
 # Lista di comandi da eseguire
 commands=(
-  "roslaunch 'ros_audio_pkg speech_recognition.launch '"
-  "roslaunch pepper_nodes pepper_bringup.launch"
-  "top"
+  "cd project_ws && source devel/setup.bash && roslaunch ros_audio_pkg speech_recognition.launch"
+  "cd project_ws && source devel/setup.bash && roslaunch rasa_ros dialogue.xml"
+  "cd chatbot && rasarun"
 )
 
 # Esegui ogni comando in un nuovo terminale
 for cmd in "${commands[@]}"; do
   gnome-terminal -- bash -c "$cmd; exec bash"
 done
-
-#roslaunch your_package second_launch_file.launch &
-
-
-wait # Wait for all background processes to finish
